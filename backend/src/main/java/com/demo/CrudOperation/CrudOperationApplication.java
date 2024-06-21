d4ffd084ff6ce1bb84eb156217a9362d267393b8
G-Bhavagna <gaddebhavagna1@gmail.com>
2024-06-21 09:03:12 +0530

    Create intial spring boot project

---
+++ backend/src/main/java/com/demo/CrudOperation/CrudOperationApplication.java
@@ -0,0 +1,13 @@
+package com.demo.CrudOperation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudOperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudOperationApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/demo/CrudOperation/CrudOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.CrudOperation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudOperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
